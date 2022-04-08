cc9f398988f46904c5c319d56ecf178d03cf8dde
nftteamdev <103230405+nftteamdev@users.noreply.github.com>
2022-04-08 09:24:46 +0700

    Add files via upload

---
+++ hardhat.config.js
@@ -0,0 +1,58 @@
+require('hardhat-deploy');
+require('hardhat-deploy-ethers');
+require("hardhat-change-network");
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan");
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: {
+    version: "0.8.12",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
+  defaultNetwork: "Shibuya",
+  namedAccounts: {
+    deployer: 0
+  },
+  networks: {
+    ropsten: {
+      url: 'https://eth-ropsten.alchemyapi.io/v2/u1hnSaxE-vlf3B8yEX_NvZ1o_GzcZAdL',
+      accounts: ['2218a64e54440561f01cd4124bb7e1f638c3109a5961cdf039e1efacd33b7505']
+    },
+	Shibuya: {
+      url: 'https://rpc.shibuya.astar.network:8545',
+      accounts: ['2218a64e54440561f01cd4124bb7e1f638c3109a5961cdf039e1efacd33b7505'],
+	    chainId: 81
+   },
+	Astar: {
+      url: 'https://rpc.astar.network:8545',
+      accounts: ['2218a64e54440561f01cd4124bb7e1f638c3109a5961cdf039e1efacd33b7505'],
+	    chainId: 592
+    }
+  },
+  etherscan: {
+	  apiKey: {
+		  ropsten: "",
+	  }
+  }
+};
